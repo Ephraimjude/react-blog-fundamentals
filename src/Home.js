3b1ee6e944b410a6527107ca1eddba605de2af69
Ephraim Jude <Ephraimjude1@gmail.com>
2023-09-09 23:49:51 +0100

    made some changes to the navbar

--- src/Home.js
+++ src/Home.js
@@ -7,19 +7,26 @@ const Home = () => {
     const { data:blogs, isPending, error} = useFetch('http://localhost:8080/blogs');
     
     return (    
-        <div key = {blogs} className="home">
-                <p className='intro'>All the Blogs</p>
-                { error && <div>{ error } </div>}
-                { isPending && <div>Loading... </div>}
-                {blogs && blogs.length === 0 && (
-                    <div className='no_blog'>
-                        <p>No blogs currently available.</p>
-                        <p>Create a new blog!</p>
-                        <button type="button"><Link to="/create" className="create-link">Create Blog</Link></button>
-                    </div>
-                )}
-                { blogs && <BlogList blogs = {blogs} /> }
+        <div className="home">
+    <p className="intro">All the Blogs</p>
+    {error && (
+        <div className="error-message">
+            {error}
         </div>
+    )}
+    {isPending && <div>Loading...</div>}
+    {blogs && blogs.length === 0 && (
+        <div className="no_blog">
+            <p>No blogs currently available.</p>
+            <p>Create a new blog!</p>
+            <button type="button">
+                <Link to="/create" className="create-link">Create Blog</Link>
+            </button>
+        </div>
+    )}
+    {blogs && <BlogList blogs={blogs} />}
+</div>
+
      );
 }
  
